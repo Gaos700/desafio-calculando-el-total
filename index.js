1759e7585cad11bee1fa9270e8879af7b934e67d
Gaos700 <gustavorellana700@gmail.com>
2025-08-22 14:03:53 -0400

    Agregando funcion suma

---
+++ index.js
@@ -0,0 +1,15 @@
+const precio = 400000
+let cantidad = 0
+
+const precioSpan = document.querySelector(".precio-inicial");
+const cantidadSpan = document.querySelector(".cantidad");
+const totalSpan = document.querySelector(".valor-total");
+
+precioSpan.textContent = precio;
+totalSpan.textContent = 0;
+
+function sumar() {
+    cantidad++;
+    cantidadSpan.textContent = cantidad;
+    totalSpan.textContent = cantidad*precio
+}
